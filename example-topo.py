d38deaa19b2111297caa22fd13b91d5ca991bb00
PenguinXuan <zwang437@ucsc.edu>
2020-04-19 19:56:30 -0700

    updated

--- example-topo.py
+++ example-topo.py
@@ -12,25 +12,25 @@ class MyTopology(Topo):
         Topo.__init__(self)
 
         # Set Up Topology Here
-        switch = self.addSwitch('s1')    ## Adds a Switch
+	switch = self.addSwitch('s1')    ## Adds a Switch
         switch = self.addSwitch('s2')
         switch = self.addSwitch('s3')
 
-        host1 = self.addHost(h1)       ## Adds a Host
-        host2 = self.addHost(h2)
-        host3 = self.addHost(h3)
-        host4 = self.addHost(h4)
-        host5 = self.addHost(h5)
-        host6 = self.addHost(h6)
+        host1 = self.addHost('h1')       ## Adds a Host
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
+        host4 = self.addHost('h4')
+        host5 = self.addHost('h5')
+        host6 = self.addHost('h6')
 
-       self.addLink(host1, s1)      ## Add a link
-       self.addLink(host3, s1)
-       self.addLink(host2, s2)
-       self.addLink(host4, s2
-       self.addLink(host5, s3)
-       self.addLink(host6, s3)
-       self.addLink(s1, s3)
-       self.addLink(s2, s3)
+	self.addLink(host1, 's1')      ## Add a link
+        self.addLink(host3, 's1')
+        self.addLink(host2, 's2')
+        self.addLink(host4, 's2')
+        self.addLink(host5, 's3')
+        self.addLink(host6, 's3')
+        self.addLink('s1', 's3')
+        self.addLink('s2', 's3')
        
 if __name__ == '__main__':
     """
